cc62e24e21317db4447a95b4909756eb914d83b1
Aarón Reyes <ar_dmg@hotmail.com>
2026-02-26 10:35:55 -0600

    Correciones y simplificaciones de código

--- src/popup.js
+++ src/popup.js
@@ -1,20 +1,16 @@
+const browserApi = window.browser || window.chrome;
+
 async function initHandler(id, value) {
     document.getElementById(id).checked = value;
     document.getElementById(id).addEventListener("click", function () {
-
-        //Use the right browser object based on the browser
-        //Firefox uses the browser object while Chrome uses chrome object
-        var browser = (window.browser)? window.browser : window.chrome;
-        
         const currentValue = document.getElementById(id).checked;
-        browser.storage.sync.set({[id]: currentValue});
-        if (browser.tabs && browser.tabs.reload) {
-            browser.tabs.reload();
-        } else if (window.chrome && chrome.tabs && chrome.tabs.reload) {
-            chrome.tabs.reload();
+        browserApi.storage.sync.set({[id]: currentValue});
+        
+        const tabsApi = browserApi.tabs || window.chrome?.tabs;
+        if (tabsApi?.reload) {
+            tabsApi.reload();
         }
     });
-    
 }
 
 async function init() {
